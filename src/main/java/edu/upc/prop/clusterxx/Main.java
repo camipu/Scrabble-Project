2eb7ddc93aad9b72dc40ee46567da4099637c32f
ToniGratacosMiralles <toni.gratacos.miralles@gmail.com>
2025-03-20 22:36:15 +0100

    Col·locar fitxa des de joc

--- src/main/java/edu/upc/prop/clusterxx/Main.java
+++ src/main/java/edu/upc/prop/clusterxx/Main.java
@@ -4,9 +4,9 @@ public class Main {
   public static void main(String[] args) {
     Joc joc = new Joc(2, "castellano");
     joc.imprimirInfoJugadors();
-    joc.colocarFitxa(1,joc.getPersona(1).obtenirFitxa(2), 2, 2);
+    joc.colocarFitxa(0,joc.getPersona(0).obtenirFitxa(0), 0, 0);
+    joc.getTaulell().imprimirTaulell();
     joc.imprimirInfoJugadors();
-//    joc.getTaulell().imprimirTaulell();
 //    joc.imprimirInfoJugadors();
   }
 
