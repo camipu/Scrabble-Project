9e220106ea745c82d462ba3ffbf511ac406a4e10
camivsantos <camila.valeria.santos@estudiantat.upc.edu>
2025-03-26 17:26:53 +0100

    Prueba

--- src/main/java/edu/upc/prop/clusterxx/Main.java
+++ src/main/java/edu/upc/prop/clusterxx/Main.java
@@ -2,7 +2,7 @@ package edu.upc.prop.clusterxx;
 
 public class Main {
   public static void main(String[] args) {
-    Joc joc = new Joc(2, "castellano");
+    Joc joc = new Joc(2, "catalán");
     joc.imprimirInfoJugadors();
     joc.colocarFitxa(0,joc.getPersona(0).obtenirFitxa(0), 0, 0);
     joc.getTaulell().imprimirTaulell();
