a831cab4f5f18ffe89629e081a149f4348a90b1b
camivsantos <camila.valeria.santos@estudiantat.upc.edu>
2025-03-27 20:52:22 +0100

    Creación de CtrDomini, nada

---
+++ src/main/java/edu/upc/prop/clusterxx/CtrDomini.java
@@ -0,0 +1,30 @@
+package edu.upc.prop.clusterxx;
+
+import java.util.List;
+
+public class CtrDomini {
+    private Taulell taulell;          // El tablero de Scrabble
+    private Diccionari diccionari; // El diccionario de palabras válidas
+    private List<Jugador> jugadors;  // Lista de jugadores
+    private int jugadorActual; // Índice del jugador que está en turno
+    private static CtrDomini instance = null;
+
+
+    public static CtrDomini getInstance() {
+        if (instance == null) {
+            instance = new CtrDomini();
+        }
+        return instance;
+    }
+
+    public void iniciarJoc(int numJugadors, String idioma, String[] noms) {
+        Joc joc = new Joc(numJugadors, idioma, noms);
+        taulell = joc.obtenirTaulell();
+        diccionari = joc.obtenirDiccionari();
+        jugadors = joc.obtenirJugadors();
+        jugadorActual = 0;
+    }
+
+}
+
+
