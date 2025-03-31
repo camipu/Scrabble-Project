7a3bbaff2548cc38d3c84396ade4bedd5b5ae541
ToniGratacosMiralles <toni.gratacos.miralles@gmail.com>
2025-03-31 16:36:42 +0200

    Ensalada

--- src/main/java/edu/upc/prop/clusterxx/Main.java
+++ src/main/java/edu/upc/prop/clusterxx/Main.java
@@ -11,14 +11,37 @@ public class Main {
     Fitxa f2 = new Fitxa("A", 3);
     Fitxa f3 = new Fitxa("S", 3);
     Fitxa f4 = new Fitxa("A", 3);
+    Fitxa f5 = new Fitxa("S", 3);
+
+    Fitxa f6 = new Fitxa("A", 3);
+    Fitxa f7 = new Fitxa("L", 3);
+    Fitxa f8 = new Fitxa("A", 3);
+    Fitxa f9 = new Fitxa("D", 3);
+    Fitxa f10 = new Fitxa("A", 3);
+    Fitxa f11 = new Fitxa("E", 3);
+    Fitxa f12 = new Fitxa("N", 3);
+    //Col·locar SALA (vertical)
     joc.colocarFitxa(f1, 3, 3);
     joc.colocarFitxa(f2, 4, 3);
     joc.colocarFitxa(f3, 5, 3);
     joc.colocarFitxa(f4, 6, 3);
+    //Col·locar la S connectora
+    joc.colocarFitxa(f5, 7, 3);
+    //Col·locar ALAS (horitzontal)
+    joc.colocarFitxa(f6, 7, 4);
+    joc.colocarFitxa(f7, 7, 5);
+    joc.colocarFitxa(f8, 7, 6);
+    joc.colocarFitxa(f9, 7, 7);
+    joc.colocarFitxa(f10, 7, 8);
+    joc.colocarFitxa(f11, 7, 1);
+    joc.colocarFitxa(f12, 7, 2);
+
+
+
     joc.obtenirTaulell().imprimirTaulell();
     joc.imprimirInfoJugadors();
     joc.obtenirTaulell().imprimirTaulell();
-    int [][] v= {{3 , 3},{4, 3},{5 , 3},{6 , 3}};
+    int [][] v= {{7 , 3}};
     HashMap<String,Integer> palabras = new HashMap<String, Integer>();
     palabras = joc.obtenirTaulell().buscaPalabrasValidas(v);
     System.out.println(palabras);
