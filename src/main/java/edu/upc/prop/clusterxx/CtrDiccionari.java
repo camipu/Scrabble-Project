8633a2223194a9945a6cd0fbc0d57c77f8af7e21
camivsantos <camila.valeria.santos@estudiantat.upc.edu>
2025-04-13 18:37:17 +0200

    no habia hecho el add, controlado Diccionari jiji

--- src/main/java/edu/upc/prop/clusterxx/CtrDiccionari.java
+++ src/main/java/edu/upc/prop/clusterxx/CtrDiccionari.java
@@ -1,4 +1,24 @@
 package edu.upc.prop.clusterxx;
 
 public class CtrDiccionari {
+    private Diccionari diccionari;
+
+    public CtrDiccionari(String idioma) {
+        this.diccionari = new Diccionari(idioma);
+    }
+
+    public boolean esParaulaValida(String paraula) {
+        return diccionari.esParaulaValida(paraula);
+    }
+
+
+    // Mètode per obtenir el nombre de paraules en el diccionari
+    public int obtenirQuantitatDeParaules() {
+        return diccionari.obtenirQuantitatDeParaules();
+    }
+
+    // Mètode per mostrar totes les paraules en el diccionari (per depuració o proves)
+    public void mostrarParaules() {
+        diccionari.mostrarParaules();
+    }
 }
