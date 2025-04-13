d4b58657497127b0111052f85ada5ac976d9bcc8
Jan <janpruna4@gmail.com>
2025-04-13 21:26:28 +0200

    contrtolador Sac

---
+++ src/main/java/edu/upc/prop/clusterxx/CtrlSac.java
@@ -0,0 +1,41 @@
+package edu.upc.prop.clusterxx;
+
+public class CtrlSac {
+    private final Sac sac;
+
+    public CtrlSac(Sac sac) {
+        this.sac = sac;
+    }
+
+    public Fitxa agafarFitxaAleatoria() {
+        return sac.agafarFitxa();
+    }
+
+    public Fitxa agafarFitxa(Fitxa fitxa) {
+        return sac.agafarFitxa(fitxa);
+    }
+
+    public void afegirFitxa(Fitxa fitxa) {
+        sac.afegirFitxa(fitxa);
+    }
+
+    public void mostrarContingut() {
+        sac.mostrarContingut();
+    }
+
+    public boolean esBuit() {
+        return sac.esBuit();
+    }
+
+    public int quantitatFitxes(Fitxa fitxa) {
+        return sac.quantitatFitxes(fitxa);
+    }
+
+    public int obtenirNumFitxes() {
+        return sac.obtenirNumFitxes();
+    }
+
+    public Sac getSac() {
+        return sac;
+    }
+}
